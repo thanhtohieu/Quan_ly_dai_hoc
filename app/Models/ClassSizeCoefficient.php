1043697a7c442b460827eea9f02eb73a68a360dc
chi-hoan <hoanlechi06@gmail.com>
2025-10-20 21:37:14 +0700

    update class size

---
+++ app/Models/ClassSizeCoefficient.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ClassSizeCoefficient extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'min_students',
+        'max_students',
+        'coefficient',
+    ];
+}
