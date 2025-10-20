ed6e4424b68b92a57a4fa3ac154bfc64824cc294
chi-hoan <hoanlechi06@gmail.com>
2025-10-20 21:57:05 +0700

    update course offer

---
+++ app/Models/CourseOffering.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class CourseOffering extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'subject_id',
+        'semester_id',
+    ];
+
+    public function subject(): BelongsTo
+    {
+        return $this->belongsTo(Subject::class);
+    }
+
+    public function semester(): BelongsTo
+    {
+        return $this->belongsTo(Semester::class);
+    }
+}
