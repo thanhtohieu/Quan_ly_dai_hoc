ab929fd7aee9b70592f25bb1d086a39f3681369c
chi-hoan <hoanlechi06@gmail.com>
2025-10-20 23:31:18 +0700

    update removing register

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -169,12 +169,6 @@
                                     <a class="nav-link" href="{{ route('login') }}">Đăng nhập</a>
                                 </li>
                             @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">Đăng ký</a>
-                                </li>
-                            @endif
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
