84daea215084848f4f61ebac3e8afebab5b1c673
Thanh Dinh <dinhthethanhthanh1@gmail.com>
2025-10-20 15:52:27 +0700

    sua loi ten file

--- app/Http/Controllers/ClassSizeCoefficientController.php
+++ app/Http/Controllers/ClassSizeCoefficientController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\ClassSizeCoefficient;
+use Database\Factories\ClassSizeCoefficientFactory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
